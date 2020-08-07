2b2c5eb24c8bb51615f93f8c7ce7f37266ce84e1
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-08-07 10:17:26 -0700

    ex1 completed

--- hashtables/ex1/README.md
+++ hashtables/ex1/README.md
@@ -29,13 +29,13 @@ output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum eq
   quadratic-runtime solution. How can we use a hash table in order to
   implement a solution with a better runtime?
 
-* Think about what we can store in the hash table in order to help us to
+* Think about what we can store in the hash table in order to help us toZ
   solve this problem more efficiently. 
 
 * What if we store each weight in the input list as keys? What would be
   a useful thing to store as the value for each key? 
 
 * If we store each weight's list index as its value, we can then check
-  to see if the hash table contains an entry for `limit - weight`. If it
+  to see if the hash table contains an entry for `limit - weight` (difference). If it
   does, then we've found the two items whose weights sum up to the
   `limit`!
--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,31 @@
-def get_indices_of_item_weights(weights, length, limit):
+def get_indices_of_item_weights(weights, length, limit): # O(n)
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # What is given to us to store inside our cache? 
+    # what if we store each weight in the input list as keys?
+        # we know that the weight = specific.key 
+    # what is the value of each key 
+        # lets store the the index of each weight as its value 
+
+
+    cache = {} 
+
+    if length == 2:
+        return(1,0)
+
+
+    for num in range(length): 
+        cache[(weights[num])] = num
+    for num in range(length):
+        difference = limit - weights[num]
+        #if the hash table contains an entry for `limit - weight`. 
+
+        if difference in cache:
+            if difference + weights[num]:
+                return(cache[difference], num)
+
 
     return None
+
+get_indices_of_item_weights([4, 6, 10, 15, 16 ], 5, 21)
